9293821b099aed0561133517339a5210a875bb25
Tommy221203 <55913204+Tommy221203@users.noreply.github.com>
2020-03-24 18:31:29 +0100

    Add project files.

---
+++ EserciziPerCas/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace EserciziPerCas
+{
+    public class Class1
+    {
+        public static bool AreEquals(string s1,string s2)
+        {
+            bool n;
+            if (s1 == s2)
+            {
+                n = true;
+            }
+            else
+            {
+                int c=0;
+                n = false;
+                for(int i=0;i<s1.Length;i++)
+                {
+                    if(s1[i]==s2[i])
+                    {
+                        c = c + 1;
+                    }
+                }
+                Console.WriteLine($"le 2 stringhe sono uguali fino al carattere numero {c}");
+            }
+            return n;
+        }
+        public static bool ContainsDoubleChar(string s)
+        {
+            bool n;
+            if (s.Contains("A") || s.Contains("E") || s.Contains("I") || s.Contains("O") || s.Contains("U"))
+            {
+                n = true;
+            }
+            else
+            {
+                n = false;
+            }
+            return n;
+        }
+        public static string Convert(string s)
+        {
+            for(int i=0;i<s.Length;i++)
+            {
+                if(i==0)
+                {
+                    Char.ToUpper(s[i]);
+                }
+                if (s[i] ==' ')
+                {
+                    Char.ToUpper(s[i + 1]);
+                }
+                else
+                {
+                    Char.ToLower(s[i + 1]);
+                }
+            }
+            return s;
+        }
+    }
+}
---
+++ ProvaConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ProvaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string res;
+            bool a;
+            Console.WriteLine("Inserisci il primo valore:");
+            string s1 = Console.ReadLine();
+            a = EserciziPerCas.Class1.ContainsDoubleChar(s1);
+            if (a == true)
+                Console.WriteLine("la stringa contiene almeno una") ;
+            res = EserciziPerCas.Class1.Convert(s1);
+            Console.WriteLine($"la stringa corretta è {res}");
+            Console.WriteLine("Inserisci il secondo valore:");
+            string s2 = Console.ReadLine();
+            a = EserciziPerCas.Class1.AreEquals(s1, s2);
+            if (a == true)
+                Console.WriteLine(0);
+            else
+            {
+                int c=0;
+                for (int i = 0; i < s1.Length; i++)
+                {
+                    if (s1[i] == s2[i])
+                    {
+                        c = c + 1;
+                    }
+                }
+                Console.WriteLine($"le 2 stringhe sono uguali fino al carattere numero {c}");
+            }
+
+
+        }
+    }
+}
